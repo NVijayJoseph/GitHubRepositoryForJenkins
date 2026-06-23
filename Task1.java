6951aaf017044441dab35d7fe016cf6cc9744b9d
NVijayJoseph <vijay.n.prof@gmail.com>
2026-06-23 16:17:16 +0530

    third comit

--- Task1.java
+++ Task1.java
@@ -1,21 +1,3 @@
-package org.task;
-
-import java.awt.AWTException;
-import java.awt.Robot;
-import java.awt.event.KeyEvent;
-import java.time.Duration;
-import java.util.Set;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 public class Task1 {
 	
 	public static void main(String[] args) throws AWTException, InterruptedException {
